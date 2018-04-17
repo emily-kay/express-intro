7fed797da957d86692e8ce2aaa0b3c7e04e2a385
Emily Kay <emilykay917@gmail.com>
2018-04-17 11:52:53 -0500

    added some requests

--- server.js
+++ server.js
@@ -3,4 +3,14 @@ const express = require('express'); //translated: go to the node module folder, 
 const app = express(); //the thing that was exported from express above, now run it
 const PORT = 5000; //all caps cause it won't change
 
-app.listen(PORT); //PORT is which entry way, can be any number between 1 and 9999, it's mostly arbitrary, occasionally it will be horrific
+app.get('/', (req, res) => {
+    res.send('Hello World'); //req stands for request, res stands for response
+});//when someone goes to localhost: 5000 this is what we want to happen
+
+app.get('/hey', (req, res) =>{
+    res.send('Howdy');
+})
+
+app.listen(PORT, function () {
+    console.log(`listening on port: ${PORT}`)
+}); //PORT is which entry way, can be any number between 1 and 9999, it's mostly arbitrary, occasionally it will be horrific
